1e3fa3f343bff01f3c8dd7cd73e2e743fd81eb4e
IsraelAzoulay <israelazoulay1@gmail.com>
2022-11-27 17:58:13 +0200

    Adding the header, total price, company name and logo.

--- main.py
+++ main.py
@@ -60,10 +60,51 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
+    # Calculating the total price and displaying at the bottom of the table on the ODF file.
+    total_sum = df["total_price"].sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt=str(total_sum), border=1, ln=1)
+
+
+    # Displaying the total sum sentence on the PDF file.
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    # Displaying the company name and logo on the PDF file.
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10)
+
+
+
+
+
 
     # Storing the pdf file that we just generated, in the 'PDFs' directory.
     pdf.output(f"PDFs/{filename}.pdf")
 
+
+
+
+
+
+
+txt_pdf = FPDF(orientation="portrait", unit="mm", format="A4")
+txt_filepaths = glob.glob("TEXTs_Files/*.txt")
+for filepath in txt_filepaths:
+    filename = Path(filepath).stem
+    name = filename.capitalize()
+
+    txt_pdf.add_page()
+    txt_pdf.set_font(family="Times", style="B", size=16)
+    txt_pdf.cell(w=50, h=8, txt=name, ln=1)
+
+txt_pdf.output("output.pdf")
 
 
 
